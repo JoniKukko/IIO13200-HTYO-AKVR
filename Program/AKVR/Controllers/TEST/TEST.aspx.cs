51f7f273a5f5be2f0938806257aaedba759862b3
Huppupelikaani <huppupelikaani@hotmail.com>
2015-11-16 12:16:49 +0200

    Test for TrafficLocationService

--- Program/AKVR/Controllers/TEST/TEST.aspx.cs
+++ Program/AKVR/Controllers/TEST/TEST.aspx.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using AKVR.Services;
 using AKVR.Services.Train;
-using System.Collections.Generic;
+using AKVR.Services.TrafficLocation;
 
 public partial class TEST : System.Web.UI.Page
 {
 
     protected void Page_Load(object sender, EventArgs e)
     {
-         
 
-        TrainService trainService = ServiceFactory.Train();
-        List<TrainModel> trains = trainService.SelectByStationShortCode("fds");
+        var trafficLocationService  = ServiceFactory.TrafficLocation();
+        var trafficLocation = trafficLocationService.SelectByStationName("Tampere asema");
+
+
+        var trainService = ServiceFactory.Train();
+        var trains = trainService.SelectByStationShortCode(trafficLocation.stationShortCode);
 
         result.Text = trains.Count.ToString();
+        
 
     }
 
