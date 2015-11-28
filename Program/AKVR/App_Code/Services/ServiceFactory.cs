eaccbda2932fef42029369f07235fc7edfcb8c99
Joni Kukko <joni.kukko@outlook.com>
2015-11-28 17:45:45 +0200

    ReasonCategory and ReasonCode and basemapper updates

--- Program/AKVR/App_Code/Services/ServiceFactory.cs
+++ Program/AKVR/App_Code/Services/ServiceFactory.cs
@@ -4,6 +4,9 @@ using AKVR.Services.TrafficLocation;
 using AKVR.Services.Assembly;
 using AKVR.Services.TrainType;
 using AKVR.Services.Operator;
+using AKVR.Services.ReasonCategory;
+using AKVR.Services.ReasonCode;
+using System.Net;
 
 namespace AKVR.Services
 {
@@ -14,7 +17,7 @@ namespace AKVR.Services
         public static TrainService Train()
         {
             Debug.WriteLine("AKVR:ServiceFactory - New Train.Service");
-            return new TrainService(new TrainMapper());
+            return new TrainService(new TrainMapper(new WebClient(), new LocalClient(), new SessionClient()));
         }
 
 
@@ -22,7 +25,7 @@ namespace AKVR.Services
         public static TrafficLocationService TrafficLocation()
         {
             Debug.WriteLine("AKVR:ServiceFactory - New TrafficLocation.Service");
-            return new TrafficLocationService(new TrafficLocationMapper());
+            return new TrafficLocationService(new TrafficLocationMapper(new WebClient(), new LocalClient(), new SessionClient()));
         }
 
 
@@ -30,7 +33,7 @@ namespace AKVR.Services
         public static AssemblyService Assembly()
         {
             Debug.WriteLine("AKVR:ServiceFactory - New Assembly.Service");
-            return new AssemblyService(new AssemblyMapper());
+            return new AssemblyService(new AssemblyMapper(new WebClient(), new LocalClient(), new SessionClient()));
         }
 
 
@@ -38,7 +41,7 @@ namespace AKVR.Services
         public static TrainTypeService TrainType()
         {
             Debug.WriteLine("AKVR:ServiceFactory - New TrainType.Service");
-            return new TrainTypeService(new TrainTypeMapper());
+            return new TrainTypeService(new TrainTypeMapper(new WebClient(), new LocalClient(), new SessionClient()));
         }
 
 
@@ -46,7 +49,23 @@ namespace AKVR.Services
         public static OperatorService Operator()
         {
             Debug.WriteLine("AKVR:ServiceFactory - New Operator.Service");
-            return new OperatorService(new OperatorMapper());
+            return new OperatorService(new OperatorMapper(new WebClient(), new LocalClient(), new SessionClient()));
+        }
+
+
+
+        public static ReasonCategoryService ReasonCategory()
+        {
+            Debug.WriteLine("AKVR:ServiceFactory - New ReasonCategory.Service");
+            return new ReasonCategoryService(new ReasonCategoryMapper(new WebClient(), new LocalClient(), new SessionClient()));
+        }
+
+
+
+        public static ReasonCodeService ReasonCode()
+        {
+            Debug.WriteLine("AKVR:ServiceFactory - New ReasonCategory.Service");
+            return new ReasonCodeService(new ReasonCodeMapper(new WebClient(), new LocalClient(), new SessionClient()));
         }
 
 
