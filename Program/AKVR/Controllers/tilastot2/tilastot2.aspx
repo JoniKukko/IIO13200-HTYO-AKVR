13d99a1077f57d384b5d4ee2bbda35d87ed66b74
Joni Kukko <joni.kukko@outlook.com>
2015-12-01 21:10:16 +0200

    tilastot2

---
+++ Program/AKVR/Controllers/tilastot2/tilastot2.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Controllers/MasterPage/MasterPage.master" AutoEventWireup="true" CodeFile="tilastot2.aspx.cs" Inherits="Controllers_tilastot2_tilastot2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="mainContentHolder" Runat="Server">
+<div class="col-sm-12">
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-header">
+                <h1>Tilastot <small>Viimeisimmät syyt</small></h1>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="form-inline">
+                <div class="form-group">
+                    <label>Päivämäärä</label>
+                    <asp:TextBox ID="datebox_date" runat="server" type="date" CssClass="form-control" value="2015-12-01" />
+                </div>
+                <asp:button ID="buttonSelectCauses" runat="server" CssClass="btn btn-success train-search-button" Text="Hae" OnClick="buttonSelectCauses_Click" />
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="row spacer-top-md">
+        <div class="col-sm-12">
+            <asp:table runat="server" ID="table_trainData" CssClass="table table-bordered">
+                <asp:TableRow>
+                    <asp:TableCell>Juna</asp:TableCell>
+                    <asp:TableCell>Tyyppi</asp:TableCell>
+                    <asp:TableCell>Asema</asp:TableCell>
+                    <asp:TableCell>Syy</asp:TableCell>
+                </asp:TableRow>
+            </asp:table>
+        </div>
+    </div>
+
+    
+</div>
+</asp:Content>
+
---
+++ Program/AKVR/Controllers/tilastot2/tilastot2.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AKVR.Services;
+using System.Globalization;
+
+public partial class Controllers_tilastot2_tilastot2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void buttonSelectCauses_Click(object sender, EventArgs e)
+    {
+
+        var fromDate = DateTime.Parse(datebox_date.Text);
+
+        var trainService = ServiceFactory.Train();
+        var trainList = trainService.SelectCausesByDate(fromDate);
+
+        var stationService = ServiceFactory.TrafficLocation();
+        var stations = stationService.SelectAll().ToDictionary(x => x.stationShortCode, x => x.stationName);
+
+        var reasonCategoryService = ServiceFactory.ReasonCategory();
+        var reasonCategorys = reasonCategoryService.SelectAll().ToDictionary(x => x.categoryCode, x => x.categoryName);
+
+        var reasonCodeService = ServiceFactory.ReasonCode();
+        var reasonCodes = reasonCodeService.SelectAll().ToDictionary(x => x.detailedCategoryCode, x => x.detailedCategoryName);
+
+        
+        foreach (var train in trainList)
+        {
+            foreach (var row in train.timeTableRows)
+            {
+                TableRow newRow = new TableRow();
+                TableCell[] newCells = new TableCell[] {
+                    new TableCell(),
+                    new TableCell(),
+                    new TableCell(),
+                    new TableCell()
+                };
+
+
+                newCells[0].Text = train.FullTrainName;
+                newCells[1].Text = train.trainCategory;
+                newCells[2].Text = stations[row.stationShortCode];
+
+                foreach (var cause in row.causes)
+                {
+                    string categoryName;
+                    string detailedCategoryName;
+
+                    reasonCategorys.TryGetValue(cause.categoryCode, out categoryName);
+                    reasonCodes.TryGetValue(cause.detailedCategoryCode != null ? cause.detailedCategoryCode : "" , out detailedCategoryName);
+
+                    newCells[3].Text += categoryName + " - " + detailedCategoryName + "<br>";
+                }
+
+                newRow.Cells.AddRange(newCells);
+                table_trainData.Rows.Add(newRow);
+            }
+        }
+        
+
+    }
+}
